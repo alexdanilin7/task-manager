20b857b792da1ef15e645480eb80882ae28fc5a4
andanilin <andanilin7@gmail.com>
2026-01-23 16:16:10 +0200

    Created query-provider

--- src/app/providers/query-provider.tsx
+++ src/app/providers/query-provider.tsx
@@ -2,7 +2,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { ReactNode } from "react";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+        refetchOnWindowFocus: false,
+        staleTime: 1000 * 60 * 5,
+      },
+    },
+});
 
 interface QueryProviderProps {
   children: ReactNode;
